36d3bc437c09695506132f67c0dd9777bd107a0a
Christoph Herzlieb <christophherzlieb@googlemail.com>
2025-05-02 18:32:44 +0200

    Refactor build_exe.py to use pathlib for path management and update version loading from pyproject.toml

--- scripts/build_exe.py
+++ scripts/build_exe.py
@@ -1,21 +1,23 @@
 import PyInstaller.__main__
-import os
+from pathlib import Path
 from release_version import VersionManager
 
-# Get the absolute path to the project root
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-version_manager = VersionManager("version.txt")
-version = version_manager.load_version()
+project_root = Path(__file__).parent.parent
+icon_path = project_root / "src" / "img" / "icon_32.ico"
 
-# Get absolute path to main.py
-main_script = os.path.join(project_root, 'main.py')
+version_manager = VersionManager(project_root / "pyproject.toml")
+current_version = version_manager.load_version()
 
 PyInstaller.__main__.run([
-    main_script,  # Your main script with absolute path
-    f'--name=Borderless-Games-and-Apps',  # Match the name expected in create_release.py
-    '--onefile',  # Create a single executable file
-    '--windowed',  # Don't show console window
-    '--clean',  # Clean cache before building
-    '--noconsole',  # No console window
-    f'--distpath={os.path.join(project_root, "dist")}',  # Specify dist directory
+    str(project_root / 'main.py'),
+    f'--name=Borderless-Games-and-Apps',
+    '--onefile',
+    '--windowed',
+    '--clean',
+    '--noconsole',
+    '--icon', str(icon_path),
+    '--add-data', f'{str(project_root / "version.txt")};.',
+    '--add-data', f'{str(icon_path)};src/img',
+    f'--distpath={str(project_root / "dist")}',
+    '--noconfirm',
 ])
